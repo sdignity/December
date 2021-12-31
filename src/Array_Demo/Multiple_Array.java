95f330a854c1df24414436003a117f7af1b4497b
Dcc <Dcc@DESKTOP-C09TUVH>
2021-12-31 16:58:06 +0530

    December 31

---
+++ src/Array_Demo/Multiple_Array.java
@@ -0,0 +1,29 @@
+package Array_Demo;
+
+public class Multiple_Array {
+
+	public static void main(String[] args) {
+		
+		//int arr[][]=new int[3][3];
+		
+		int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
+
+		//arr[0][0] = 1;
+		//arr[0][1] = 2;
+		//arr[1][0] = 3;
+		//arr[1][1] = 4;
+		//arr[2][0] = 5;
+		//arr[2][1] = 6;
+
+		for
+			(int i=0; i <arr.length; i++)
+		{
+		for	(int j = 0; j <arr.length; j++)
+				
+			
+	    System.out.print(arr[i][j]+" ");
+	    System.out.println();
+		}
+	}
+	}
+
---
+++ src/Array_Demo/Single_Array.java
@@ -0,0 +1,25 @@
+package Array_Demo;
+
+
+public class Single_Array {
+
+	public static void main(String[] args) {
+		
+	//	int arr[] = new int[3];
+		
+		int arr[]={1,2,3};
+		
+	//	arr[0]=1;
+	//	arr[1]=2;
+	//	arr[2]=3;
+		
+		
+		for(int i=0; i<arr.length;i++)
+	
+			
+		System.out.println(arr[i]);
+		
+		
+	}
+
+}
